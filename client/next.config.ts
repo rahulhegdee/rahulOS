93e97f983e8779e0af816c5dae890058480abc5d
Rahul Hegde <rahulh.hegde@gmail.com>
2024-12-25 13:07:59 -0800

    set up next and MDX

---
+++ client/next.config.ts
@@ -0,0 +1,13 @@
+import type { NextConfig } from "next";
+import createMDX from "@next/mdx";
+
+const nextConfig: NextConfig = {
+	/* config options here */
+	pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
+};
+
+const withMDX = createMDX({
+	// Add markdown plugins here, as desired
+});
+
+export default withMDX(nextConfig);
--- client/src/App.tsx
+++
@@ -1,9 +0,0 @@
-function App() {
-	return (
-		<>
-			<h1>rahulOS</h1>
-		</>
-	);
-}
-
-export default App;
